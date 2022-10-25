bdd311ffb38eecd3c4d261f5e8195e8e40dddbb5
Carlos Eduardo <carlos_eduardo_lab2dev@whirlpool.com>
2022-10-25 11:49:12 -0300

    fix: infinite loop in lambda trigger

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,11 @@ export const lambdaHandler = async (event: S3Event) => {
   const bucket = event.Records[0].s3.bucket.name;
   const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
 
+  if (key.includes('Index-Manual')) {
+    console.log('Process has ended');
+    return;
+  }
+
   const params: GetObjectCommandInput = {
     Bucket: bucket,
     Key: key,
