165e348fcbfd70496a9dcb5aabfe9e938e6293fe
Carlos Eduardo <carlos_eduardo_lab2dev@whirlpool.com>
2022-10-28 10:08:32 -0300

    feat(csv): format compiled indexes csv

--- src/services/convert-json-to-csv.service.ts
+++ src/services/convert-json-to-csv.service.ts
@@ -1,10 +1,29 @@
 import { IndexManual } from '../entities/index-manual.entity';
 
 const convertJSONToCSV = async (indexes: IndexManual[]) => {
-  const csvValues = indexes.map((index) => JSON.stringify(Object.values(index)));
+  const formattedIndexes = indexes.map((index) => ({
+    rule_id: index.rule_id,
+    ativo: index.ativo,
+    SKU: index.SKU,
+    utmi: index.utmi,
+    pricetable_id: index.pricetable_id,
+    fator: index.fator,
+    benchmark: index.benchmark,
+    ignorar_travas: index.ignorar_travas,
+    benchmark_1P: index.benchmark_1P,
+    benchmark_fallback: index.benchmark_fallback,
+    fator_fallback: index.fator_fallback,
+    trava_min: index.trava_min,
+    trava_max: index.trava_max,
+    loja_concorrente: index.loja_concorrente,
+    from_price: index.from_price,
+    cluster_loja: index.cluster_loja,
+  }));
+
+  const csvValues = formattedIndexes.map((index) => JSON.stringify(Object.values(index)));
 
   // Add fields to the beginning of array
-  const csvFields = JSON.stringify(Object.keys(indexes[0]));
+  const csvFields = JSON.stringify(Object.keys(formattedIndexes[0]));
   csvValues.unshift(csvFields);
 
   const csv = csvValues.join('\n')
